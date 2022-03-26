298867189679511bb737417b72111dd0b96790c6
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-25 18:29:07 -0700

    after a big bughunt adventure with Madden, things are looking good! Renderpoll function storing all the right info, state getting reset where it should be, and polls successfully getting added to past polls div. Sign in and sign up are also working and redirecting appropriately.

--- polls/polls.js
+++ polls/polls.js
@@ -1,5 +1,5 @@
 import { renderPoll } from '../render-utils.js';
-import { getPolls, createPoll, getUser, checkAuth, redirectToPolls, signUpUser, signInUser, logout } from '../fetch-utils.js';
+import { getPolls, createPoll, checkAuth, logout } from '../fetch-utils.js';
 
 const currentPollEl = document.querySelector('#current-poll-container');
 const pastPollsEl = document.querySelector('#past-polls-container');
@@ -25,12 +25,14 @@ const logoutButton = document.getElementById('logout-button');
 
 checkAuth();
 
+let pollState = {
+    title : '',
+    name1 : '',
+    name2 : '',
+    score1 : 0,
+    score2 : 0,
 
-let title = '';
-let name1 = '';
-let name2 = '';
-let score1 = 0;
-let score2 = 0;
+};
 
 
 pollForm.addEventListener('submit', (e) => {
@@ -38,9 +40,10 @@ pollForm.addEventListener('submit', (e) => {
 
     const data = new FormData(pollForm);
 
-    title = data.get('poll-name-input');
-    name1 = data.get('option-one-input');
-    name2 = data.get('option-two-input');
+    pollState.title = data.get('poll-name-input');
+    pollState.name1 = data.get('option-one-input');
+    pollState.name2 = data.get('option-two-input');
+    
 
     // questionInput.value = '';
     // optionOneInput.value = '';
@@ -55,32 +58,55 @@ pollForm.addEventListener('submit', (e) => {
 
 
 oneAddButton.addEventListener('click', () => {
-    score1++;
+    pollState.score1++;
     displayCurrentPollEl();
     //function to displayCurrentPolls
 });
 
 oneSubtractButton.addEventListener('click', () => {
-    score1--;
+    pollState.score1--;
     displayCurrentPollEl();
     //function to displayCurrentPolls
 });
 
 twoAddButton.addEventListener('click', () => {
-    score2++;
+    pollState.score2++;
     displayCurrentPollEl();
     //function to displayCurrentPolls
 });
 
 twoSubtractButton.addEventListener('click', () => {
-    score1--;
+    pollState.score1--;
     displayCurrentPollEl();
     //function to displayCurrentPolls
 });
 
 resetPollButton.addEventListener('click', async () => {
-
     
+    const pastPoll = {
+        question: pollState.title,
+        option_1: pollState.name1,
+        score_1: pollState.score1,
+        option_2: pollState.name2,
+        score_2: pollState.score2,
+    };
+    
+    await createPoll(pastPoll);
+
+    await displayAllPolls();
+
+    pollState.title = '';
+    pollState.name1 = '';
+    pollState.name2 = '';
+    pollState.score1 = 0;
+    pollState.score2 = 0;
+
+    displayCurrentPollEl();
+});
+
+
+logoutButton.addEventListener('click', () => {
+    logout();
 });
 
 
@@ -88,21 +114,36 @@ resetPollButton.addEventListener('click', async () => {
 function displayCurrentPollEl() {
     currentPollEl.textContent = '';
     
-    optionOneHeader.textContent = name1;
-    optionTwoHeader.textContent = name2;
+    optionOneHeader.textContent = pollState.name1;
+    optionTwoHeader.textContent = pollState.name2;
     
-    // const newPoll = {
-    //     question: title,
-    //     option_1: name1,
-    //     score_1: score1,
-    //     option_2: name2,
-    //     score_2: score2,
-    // };
+    const newPoll = {
+        question: pollState.title,
+        option_1: pollState.name1,
+        score_1: pollState.score1,
+        option_2: pollState.name2,
+        score_2: pollState.score2,
+    };
     
         // call the render game function to create a game element
-    const pollEl = renderPoll(title, name1, score1, name2, score2);
+    const pollEl = renderPoll(newPoll);
         
         // append the element to the cleared out current game div
     currentPollEl.append(pollEl);
 }
 
+async function displayAllPolls() {
+    pastPollsEl.textContent = '';
+
+    const polls = await getPolls();
+    
+    for (let poll of polls) {
+        const pollEl = renderPoll(poll);
+        
+        pastPollsEl.append(pollEl);
+    }
+
+}
+
+displayCurrentPollEl();
+displayAllPolls();
--- render-utils.js
+++ render-utils.js
@@ -1,4 +1,4 @@
-export function renderPoll(aTitle, anOption1, aScore1, anOption2, aScore2) {
+export function renderPoll(poll) {
     const pollDiv = document.createElement('div');
     const titleEl = document.createElement('h3');
     const optionOneEl = document.createElement('h4');
@@ -6,16 +6,18 @@ export function renderPoll(aTitle, anOption1, aScore1, anOption2, aScore2) {
     const optionTwoEl = document.createElement('h4');
     const scoreTwoEl = document.createElement('p');
 
-    titleEl.textContent = aTitle;
-    optionOneEl.textContent = anOption1;
-    scoreOneEl.textContent = aScore1;
-    optionTwoEl.textContent = anOption2;
-    scoreTwoEl.textContent = aScore2;
+    titleEl.textContent = poll.question;
+    optionOneEl.textContent = poll.option_1;
+    scoreOneEl.textContent = poll.score_1;
+    optionTwoEl.textContent = poll.option_2;
+    scoreTwoEl.textContent = poll.score_2;
 
+    console.log(poll, 'yep');
+    console.log(titleEl, optionOneEl, scoreOneEl, optionTwoEl, scoreTwoEl, 'nope');
+    
     pollDiv.append(titleEl, optionOneEl, scoreOneEl, optionTwoEl, scoreTwoEl);
-
+    
     pollDiv.classList.add('.poll');
-
     return pollDiv;
 
 }
